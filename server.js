507e0facbe538a6d562b4bb2d7f25f9a24557d6b
Brad Hodge <bhodge166@gmail.com>
2022-03-10 22:39:48 -0700

    updated port information, added package-lock.json to ignore

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require("path");
 const { randomUUID } = require("crypto");
 const fs = require("fs");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
