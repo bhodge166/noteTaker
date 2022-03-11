bbbd5f5401877d7ab7ced5d56cf8c9f5bfc5bdad
Brad Hodge <bhodge166@gmail.com>
2022-03-10 22:28:59 -0700

    added functionality to post and rerwrite to db. Added function to delete by id. Updated get function to return read file

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const db = require("./db/db.json");
 const app = express();
 const path = require("path");
+const { randomUUID } = require("crypto");
+const fs = require("fs");
 const PORT = 3001;
 
 app.use(express.urlencoded({ extended: true }));
@@ -15,8 +17,72 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
-app.get("/api/notes", (req, res) => res.json(db));
-app.post("/api/notes", (req, res) => console.log("test"));
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const readNotes = JSON.parse(data);
+      res.json(readNotes);
+    }
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received`);
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: randomUUID(),
+    };
+
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedNotes = JSON.parse(data);
+        parsedNotes.push(newNote);
+
+        fs.writeFile("./db/db.json", JSON.stringify(parsedNotes), (err) => {
+          if (err) {
+            console.err(err);
+          } else {
+            console.info("Successfully updated notes");
+          }
+        });
+      }
+    });
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    console.log(response);
+    res.json(response);
+  } else {
+    res.json("Error posting note");
+  }
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const requestedDelete = req.params.id;
+  for (let i = 0; i < db.length; i++) {
+    if (requestedDelete === db[i].id) {
+      db.splice(i, 1);
+      fs.writeFile("./db/db.json", JSON.stringify(db), (err) => {
+        if (err) {
+          console.err(err);
+        } else {
+          console.info("file rewritten");
+        }
+      });
+    }
+  }
+  res.json("Note successfuly deleted");
+});
 
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
